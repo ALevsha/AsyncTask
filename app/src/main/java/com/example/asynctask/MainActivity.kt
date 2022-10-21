9d518f2c00f2ecfc603665536b5edf34a8f34066
ALevsha <levsha26r@gmail.com>
2022-10-21 14:10:24 +0300

    add analog realization coroutine

--- app/src/main/java/com/example/asynctask/MainActivity.kt
+++ app/src/main/java/com/example/asynctask/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.asynctask
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,7 @@ import kotlinx.coroutines.GlobalScope
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,22 +25,91 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.button.setOnClickListener {
-
+/*
+            */
             /**
              * создается сфера действия в рамках жизненного цикла приложения, каманды в которой
              * выполняются отдельно от главного потока, если умрет activity, запрос также будет
              * остановлен
-             */
+             *//*
             lifecycleScope.launch {
                 loadData()
+            }*/
+            /**
+             * Корутины в общем случае являются оберткой над программированием с использованием
+             * callback'ов, только используется не thread, а Handler(Looper)
+             * */
+            loadDataWithoutCoroutine()
+        }
+    }
+
+    /**
+     * реализация аналогичная корутинам (грубо говоря, смысл один и тот же)
+     * */
+
+    private fun loadDataWithoutCoroutine(step: Int = 0, obj: Any? = null) {
+        when (step) {
+            /**
+             * код в данном случае делится на 3 блока:
+             * сперва синхронно идет код до использования первого callback'а,
+             * далее до второго,
+             * а после продолжается до конца программы
+             * Примечательно, что за поток не приостанавливается (нет применения Thread.sleep())
+             */
+            0 -> {
+                Log.d("MainActivity", "Load started")
+                binding.progressBar.isVisible = true
+                binding.button.isEnabled = false
+                loadCityWithoutCoroutine {
+                    loadDataWithoutCoroutine(1, it)
+                }
+            }
+            1 -> {
+                val city = obj as String
+                binding.tvCity.text = city
+                loadTempWithoutCoroutine(city) {
+                    loadDataWithoutCoroutine(2, it)
+                }
+            }
+            2 -> {
+                val temp = obj as Int
+                binding.tvTempreture.text = temp.toString()
+                binding.progressBar.isVisible = false
+                binding.button.isEnabled = true
+                Log.d("MainActivity", "Load finished")
             }
         }
     }
 
+    private fun loadTempWithoutCoroutine(city: String, callback: (Int) -> Unit) {
+        runOnUiThread {
+            Toast.makeText(
+                this,
+                "Loading temperature for city : ${city}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke(17)
+        }, 5000)
+    }
 
+    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
+        thread {
+            Handler(Looper.getMainLooper()).postDelayed({
+                runOnUiThread {
+                    callback.invoke("Moscow")
+                }
+            }, 5000)
+        }
+    }
+
+/*
+    */
     /**
      * для использования корутины функция помечается модификатором suspend
-     * */
+     * *//*
+
     private suspend fun loadData() {
         Log.d("MainActivity", "Load started")
         binding.progressBar.isVisible = true
@@ -50,7 +122,6 @@ class MainActivity : AppCompatActivity() {
         binding.button.isEnabled = true
         Log.d("MainActivity", "Load finished")
     }
-
 
     private suspend fun loadTemp(city: String): Int {
         Toast.makeText(
@@ -67,5 +138,6 @@ class MainActivity : AppCompatActivity() {
         delay(5000)
         return "Moscow"
     }
+*/
 
 }
